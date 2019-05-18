813d1e840a7410ad13d4b2f304c9bc60ce5696cd
Tim de Boer <timdeboer08@outlook.com>
2019-05-18 17:57:30 +0200

    Html js start

---
+++ Homework/Week_6/linked.js
@@ -0,0 +1,17 @@
+// Drugsgebruik per eu land -- datamap
+// Crime rate per eu land -- barchart
+
+// Aantal vluchteling
+// Landen
+// Afkomst land vluchteling
+// Hoeveelheid per land van afkomst
+
+// Drugsgebruik
+// Per land
+// Afkomst van drugs, productieland
+// Hoeveelheid per land van afkomst
+
+/*
+Name: Tim de Boer
+Studentnummer: 11202351
+*/
